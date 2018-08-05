d6e96d481844f9893a3d4c15e59f86cb123e235d
amir <am5050@gmail.com>
2018-08-05 19:29:30 +0300

    .

--- test/unity/test/TestProductionCode.c
+++ test/unity/test/TestProductionCode.c
@@ -1,9 +1,11 @@
-#include "bme280.c"
+// #include "bme280.h"
 #include "jumper.h"
 #include "main.h"
 #include "unity.h"
-#include "button.h"
+#include "Func.h"
 
+
+extern void  parse_sensor_data(const uint8_t *reg_data, struct bme280_uncomp_data *uncomp_data);
 
 #define BUTTON1 (13)
 
@@ -25,9 +27,13 @@ void test_CallTheButtonCallback(void)
 
 void test_SetButtonPinLevel(void) 
 {
-    // TEST_ASSERT_FALSE(button_callback_callad);
-    // jumper_sudo_set_pin_level(BUTTON1, 0);
-    // TEST_ASSERT_TRUE(button_callback_callad); //todo: uncomment
+    TEST_ASSERT_FALSE(button_callback_callad);
+    button_configuration();
+    jumper_sudo_set_pin_level(BUTTON1, 0);
+    jumper_sudo_set_pin_level(BUTTON1, 1);
+            // nrf_delay_ms(1000);
+
+    TEST_ASSERT_TRUE(button_callback_callad); //todo: uncomment
 }
 
 void test_parse_sensor_data(void) 
